ad86d870f77904d42894efaa2c0f4532e54b043d
s5245510 <alexleung9407@gmail.com>
2023-04-02 14:52:04 +1000

    Initial Commit

---
+++ A1/A1App.swift
@@ -0,0 +1,17 @@
+//
+//  A1App.swift
+//  A1
+//
+//  Created by Tsz Hoi Leung on 2/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct A1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
