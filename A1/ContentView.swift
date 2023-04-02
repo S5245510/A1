9b16738af39c7f2449714ddae025b4332ad0f653
s5245510 <alexleung9407@gmail.com>
2023-04-02 23:28:02 +1000

    format

--- A1/ContentView.swift
+++ A1/ContentView.swift
@@ -34,7 +34,6 @@ struct ContentView: View {
                         .foregroundColor(.green)
                     }
                     .padding([.top, .leading])
-                    
                 }
             }
             .listStyle(.plain)
