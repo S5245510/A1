5fdec39a5fbddb5e7cf54b15a771a3afb9907811
s5245510 <alexleung9407@gmail.com>
2023-04-02 14:55:09 +1000

    added checklist with 4 items and 2 checkmarks

--- A1/ContentView.swift
+++ A1/ContentView.swift
@@ -2,20 +2,36 @@
 //  ContentView.swift
 //  A1
 //
-//  Created by Tsz Hoi Leung on 2/4/2023.
+//  Created by Tsz Hoi Leung on 20/3/2023.
 //
 
 import SwiftUI
 
+let items = [
+    ["Item 1", "checkmark"],
+    [ "Item 2", "checkmark"],
+    ["Item 3", ""],
+    [ "Item 4",""]
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(items, id:\.self){ item in
+                    HStack {
+                        Text(item[0])
+                        Spacer()
+                        Image(systemName:item[1])
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Checklist")
+                }
+            }
         }
-        .padding()
     }
 }
 
